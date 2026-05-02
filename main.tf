edec5b04cf7309e14cdcc40bcbecfe0e659b036b
Racem Moalla <moallaracem@gmail.com>
2026-05-02 03:21:02 +0200

    corr

--- main.tf
+++ main.tf
@@ -7,22 +7,3 @@ resource "google_storage_bucket" "my-bucket" {
   uniform_bucket_level_access = true
 }
 
-resource "google_compute_instance" "vm" {
-  name ="docker-vm"
-  machine_type = "e2-micro"
-  project = "920722724682"
-  zone = "us-central1-a"
-  boot_disk { 
-    initialize_params {
-      image  = "ubuntu-os-cloud/ubuntu-2204-lts"
-    }
-  }
-
-  network_interface {
-    network = "default"
-  
-    access_config{
-    }
-  }
- 
-}
