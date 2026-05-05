33717da2ceade7c389650a2d9a54c295cdc25f82
racemmoalla01 <racem.moalla@gmail.com>
2026-05-05 02:12:13 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,6 @@
-output "vm_ip" {
-  value = google_compute_instance.vm.network_interface[0].access_config[0].nat_ip
+output "vm_ips" {
+  value = [
+    for vm in google_compute_instance.vm :
+    vm.network_interface[0].access_config[0].nat_ip
+  ]
 }
