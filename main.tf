aa6d01206d0125a7a1a7b76b1983559211e82ad2
racemmoalla01 <racem.moalla@gmail.com>
2026-05-05 02:12:01 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,5 +19,26 @@ resource "google_compute_instance" "vm" {
   metadata = {
     ssh-keys = "ubuntu:${var.public_ssh_key}"
   }
+resource "google_compute_instance" "vm" {
+  name ="docker-vm2"
+  machine_type = "e2-micro"
+  project = "920722724682"
+  zone = "us-central1-a"
+  boot_disk {
+    initialize_params {
+      image  = "ubuntu-os-cloud/ubuntu-2204-lts"
+    }
+  }
 
+  network_interface {
+    network = "default"
+
+    access_config{
+    }
+  }
+  metadata = {
+    ssh-keys = "ubuntu:${var.public_ssh_key}"
+  }
+
+}
 }
